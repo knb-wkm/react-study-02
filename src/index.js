35adec18217064b15a28968428de6fbd9ed7a775
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-07-19 17:53:43 +0900

    sample

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 const App = () => {
+  const title = "react sample";
+  const books = [
+    {name: "react tutorial", price: 3000},
+    {name: "redux tutorial", price: 6000},
+  ];
+
+  const renderBook = (book) => {
+    const expensive = book.price > 5000 ? "!!!" : "";
+
+    return (
+      <ul>
+        <li>{book.name}</li>
+        <li>{book.price}{expensive}</li>
+      </ul>
+    );
+  };
+
   return (
     <div>
-      app
+      <h2>{title}</h2>
+      {books.map(book => renderBook(book))}
     </div>
   );
 };
