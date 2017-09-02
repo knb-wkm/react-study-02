842fea423b463fa357491f6ad4322bf45905f282
y_wakayama <y_wakayama@y-wakayamanoMacBook-Pro.local>
2017-09-02 20:16:41 +0900

    addTodo reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,12 +1,39 @@
-const double = (n) => {
-  return n * 2;
+const initialState = [
+  {id: 1, name: "react勉強する", done: true},
+  {id: 2, name: "更にreactを勉強する", done: false}
+];
+
+const todoReducer = (state = initialState, action) => {
+  switch ( action.type ) {
+  case "ADD_TODO":
+    // [ ...ary, obj ] は ary.dconcat(obj)のシンタックスシュガー
+    return [...state, action.todo];
+  default:
+    return state;
+  }
 };
 
-describe("double関数のテスト", () => {
-  it("引数を2倍したものを返却する", () => {
-    const result = double(2);
-    const expected = 4;
+const addTodo = (todo) => {
+  return {
+    type: "ADD_TODO",
+    todo: todo
+  };
+};
 
+describe("todoReducerのテスト", () => {
+
+  it("ADD_TODO", () => {
+    const newTodo = { id: 3, name: "reducerを勉強する", done: false };
+    const result = todoReducer(initialState, addTodo(newTodo));
+
+    // 期待する値はinitialStateにnewTodoをconcatしたもの
+    const expected = [
+      {id: 1, name: "react勉強する", done: true},
+      {id: 2, name: "更にreactを勉強する", done: false},
+      newTodo
+    ];
     expect(result).toEqual(expected);
   });
 });
+
+  
